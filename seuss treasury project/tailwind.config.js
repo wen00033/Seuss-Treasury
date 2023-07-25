5b43660ac3ddcb7d81b5a4940c305efbba8bdc1c
Chi-hung Wen <wen00033@algonquinlive.com>
2023-07-25 12:08:35 -0400

    updated

--- seuss treasury project/tailwind.config.js
+++ seuss treasury project/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
